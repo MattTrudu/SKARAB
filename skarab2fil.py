fe5f0c56a8e7efc4936f7cf12d8aec572c9cdc44
Matt Trudu <trudumatteo@outlook.com>
2023-12-19 13:54:39 +0100

    Changes

--- skarab2fil.py
+++ skarab2fil.py
@@ -47,27 +47,27 @@ def get_tstamp(filename, start = 0):
 
     return tstamp
 
-    def get_sampling_time(filename, nchan = 4096, npol = 2):
-            file_sardara_obs = open(filename, 'rb')
+def get_sampling_time(filename, nchan = 4096, npol = 2):
+        file_sardara_obs = open(filename, 'rb')
 
-            N_spectra = 100
+        N_spectra = 100
 
-            array_byte_seconds = np.zeros(N_spectra)
+        array_byte_seconds = np.zeros(N_spectra)
 
-            byte_seconds_from_1970_spectrum_0    = file_sardara_obs.read(8)
-            byte_microseconds_spectrum_0         = file_sardara_obs.read(8)
-            array_byte_seconds[0] = get_time_stamp_from_spectrum(byte_seconds_from_1970_spectrum_0, byte_microseconds_spectrum_0)
+        byte_seconds_from_1970_spectrum_0    = file_sardara_obs.read(8)
+        byte_microseconds_spectrum_0         = file_sardara_obs.read(8)
+        array_byte_seconds[0] = get_time_stamp_from_spectrum(byte_seconds_from_1970_spectrum_0, byte_microseconds_spectrum_0)
 
-            for k in range(1, N_spectra):
-                    file_sardara_obs.seek( k * (npol*nchan + 16))
-                    byte_seconds_from_1970    = file_sardara_obs.read(8)
-                    byte_microseconds         = file_sardara_obs.read(8)
-                    array_byte_seconds[k] = get_time_stamp_from_spectrum(byte_seconds_from_1970, byte_microseconds)
+        for k in range(1, N_spectra):
+                file_sardara_obs.seek( k * (npol*nchan + 16))
+                byte_seconds_from_1970    = file_sardara_obs.read(8)
+                byte_microseconds         = file_sardara_obs.read(8)
+                array_byte_seconds[k] = get_time_stamp_from_spectrum(byte_seconds_from_1970, byte_microseconds)
 
-            file_sardara_obs.close()
+        file_sardara_obs.close()
 
-            for k in range(1, N_spectra):
-                    print("array_byte_seconds[%d] - array_byte_seconds[%d] = %.4e s" % (k, k-1, array_byte_seconds[k]-array_byte_seconds[k-1] ))
+        for k in range(1, N_spectra):
+                print("array_byte_seconds[%d] - array_byte_seconds[%d] = %.4e s" % (k, k-1, array_byte_seconds[k]-array_byte_seconds[k-1] ))
 
 
 
