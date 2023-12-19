9d66d02f98fdd417fd116be1a39bb09843b8c336
Matt Trudu <trudumatteo@outlook.com>
2023-12-19 14:49:53 +0100

    Changes

--- skarab2fil.py
+++ skarab2fil.py
@@ -101,7 +101,7 @@ if __name__ == "__main__":
 
     tstamps = get_all_timestamps(filename)
 
-    dt = np.diff(tstamps)
+    dts = np.diff(tstamps)
 
-    for tstamp in tstamps[0:100]:
-        print((dt * u.s).to( u.s))
+    for dt in dts[0:100]:
+        print((dt * u.s).to( u.us))
