eddf3fff6d3c5712e7094f6d16530bfa5d9958a2
Matt Trudu <trudumatteo@outlook.com>
2023-12-19 11:01:04 +0100

    Changes

--- skarab2fil.py
+++ skarab2fil.py
@@ -95,4 +95,12 @@ if __name__ == "__main__":
 
     filename = args.filename
 
-    get_all_timestamps(filename)
+    tstamp = get_tstamp(filename, start = 0)
+    print("0",tstamp.mjd)
+    tstamp = get_tstamp(filename, start = 8192)
+    print("8192", tstamp.mjd)
+    tstamp = get_tstamp(filename, start = 8192 + 16)
+    print("8192 + 16", tstamp.mjd)
+
+
+    #get_all_timestamps(filename)
